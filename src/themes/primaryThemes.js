82df3eaf45c346cb6c2b8498092cd593de43ed23
scarlettscoville <scarlettscoville@gmail.com>
2022-05-25 19:12:59 -0400

    first commit

---
+++ src/themes/primaryThemes.js
@@ -0,0 +1,23 @@
+export const themeOptions = {
+    palette: {
+      type: 'dark',
+      primary: {
+        main: '#086490',
+      },
+      secondary: {
+        main: '#F40290',
+      },
+      info: {
+        main: '#2196f3',
+      },
+      success: {
+        main: '#00e9d6',
+      },
+      error: {
+        main: '#F9080C',
+      },
+      warning: {
+        main: '#FBE81A',
+      },
+    },
+  };
